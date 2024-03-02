ec0bace714ff27b4b5c2f7970ef8042804f28ef7
Chamy619 <yd95069506@naver.com>
2024-03-02 14:30:00 +0900

    feat: 설정 파일 읽기

---
+++ config.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Notifies []Notify `yaml:"notifies"`
+}
+
+type Notify struct {
+	Files []GithubActionFile `yaml:"files"`
+}
+
+type GithubActionFile struct {
+	Name string `yaml:"name"`
+	Type string `yaml:"type"`
+	Added bool `yaml:"added"`
+	Modified bool `yaml:"modified"`
+	Removed bool `yaml:"removed"`
+	Message string `yaml:"message"`
+}
+
+func NewConfig(filename string) (*Config, error) {
+	var config Config
+
+	yamlFile, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, errors.New("file is not readable")
+	}
+
+	err = yaml.Unmarshal([]byte(yamlFile), &config)
+	if err != nil {
+		return nil, errors.New("yaml file is not invalid")
+	}
+	fmt.Println(config.Notifies[0].Files)
+
+	return &config, nil
+}
---
+++ config_test.go
@@ -0,0 +1,49 @@
+package main
+
+import "testing"
+
+func TestNewConfig(t *testing.T) {
+	filename := "config.yaml.sample"
+	config, err := NewConfig(filename)
+
+	if err != nil {
+		t.Errorf("NewConfig fail: %s", err)
+	}
+	
+	gotName := config.Notifies[0].Files[0].Name
+	wantName := "apps/master-api/config.ini.sample"
+	AssertEqual(t, wantName, gotName)
+
+	gotType := config.Notifies[0].Files[0].Type
+	wantType := "github-action"
+	AssertEqual(t, wantType, gotType)
+
+	gotAdded := config.Notifies[0].Files[0].Added
+	wantAdded := false
+	AssertEqual(t, wantAdded, gotAdded)
+	
+	gotModified := config.Notifies[0].Files[0].Modified
+	wantModified := true
+	AssertEqual(t, wantModified, gotModified)
+
+	gotRemoved := config.Notifies[0].Files[0].Removed
+	wantRemoved := false
+	AssertEqual(t, wantRemoved, gotRemoved)
+
+	gotMessage := config.Notifies[0].Files[0].Message
+	wantMessage := "apps/master-api/config.ini.sample file is changed"
+	AssertEqual(t, wantMessage, gotMessage)
+}
+
+func TestNewConfigNotExistFile(t *testing.T) {
+	filename := "notexists.yaml"
+	_, err := NewConfig(filename)
+	AssertEqual(t, "file is not readable", err.Error())
+}
+
+func AssertEqual(t *testing.T, want interface{}, got interface{}) {
+	t.Helper()
+	if want != got {
+		t.Errorf("%v is not equal %v", want, got)
+	}
+}
