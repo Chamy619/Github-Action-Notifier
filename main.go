42b96efb72812e8a38719f9fd70ae2411df273b0
Chamy619 <yd95069506@naver.com>
2024-03-06 16:19:16 +0900

    feat: CLI, HTTP 추가

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("Welcome github action notifier")
+	configFilenamePtr := flag.String("config", "config.yaml", "config file name")
+	hostPtr := flag.String("host", "localhost", "host")
+	portPtr := flag.Int64("port", 8080, "port")
+
+	flag.Parse()
+	
+	config, err := NewConfig(*configFilenamePtr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	configHandler := func(writer http.ResponseWriter, request *http.Request) {
+		configStr := fmt.Sprintf("%v", config)
+		writer.Write([]byte(configStr))
+	}
+
+	http.HandleFunc("/config", configHandler)
+
+	http.ListenAndServe(*hostPtr + ":" + strconv.FormatInt(*portPtr, 10), nil)
 }
